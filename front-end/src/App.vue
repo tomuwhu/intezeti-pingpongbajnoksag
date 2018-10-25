937d2921c1d108b33983c23da604b7e3e895e283
Tamás Németh <tomuwhu@gmail.com>
2018-10-25 15:32:25 +0200

    .

--- front-end/src/App.vue
+++ front-end/src/App.vue
@@ -4,11 +4,20 @@
      
     </div>
     <HelloWorld msg="Üdvözöllek újra az intézeti ping-pong bajonokságban!"/>
+    <div id="mynetwork"></div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import vis from 'vis'
+
+var nodes = {}
+var edges = {}
+var container = {}
+var data = {}
+var options = {}
+var network = {}
 
 export default {
   name: 'app',
@@ -18,7 +27,40 @@ export default {
   }),
   components: {
     HelloWorld
-  }
+  },
+  mounted: function () {
+
+      nodes = new vis.DataSet([
+        { id: 1, label: 'Node 1' },
+        { id: 2, label: 'Node 2' },
+        { id: 3, label: 'Node 3' },
+        { id: 4, label: 'Node 4' }
+      ])
+
+      edges = new vis.DataSet([
+        { from: 1, to: 3 },
+        { from: 1, to: 2 },
+        { from: 2, to: 4 },
+        { from: 1, to: 4 },
+        { from: 3, to: 4 },
+      ])
+
+      container = document.getElementById('mynetwork');
+      data = {
+        nodes: nodes,
+        edges: edges
+      }
+
+      options = {
+        edges: {
+          arrows: {
+            to: {enabled: true, scaleFactor:  .7, type:'arrow'}
+          }
+        }
+      }
+
+      network = new vis.Network(container, data, options);
+    }
 }
 </script>
 
@@ -35,5 +77,9 @@ h1 {
 }
 #app {
   margin:20px;
+}
+#mynetwork {
+  width: 90%;
+  height: 600px;
 }
 </style>
